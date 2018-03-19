42058e3e44587279601cd8c71a90ebb5ad376745
theo.segard <theo.segard@lecole-ldlc.com>
2018-03-19 01:38:05 +0100

    map folder

--- TD.py
+++ TD.py
@@ -1,3 +1,14 @@
 import pygame
 from tilemap import *
+import pytmx
+from pytmx.util_pygame import load_pygame
 #installer pytmx
+
+# initialize pygame
+pygame.init()
+clock = pygame.time.Clock()
+
+# create game display
+game_display = pygame.display.set_mode((800, 800))
+pygame.display.set_caption("Collision detection with Tiled and pytmx")
+pytmx_map = load_pygame("test.tmx")
