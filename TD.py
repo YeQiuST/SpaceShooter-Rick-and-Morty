599293dd53929f50813ba93a2380e859e7d5b51f
enzo.chavaete <enzo.chavaete@lecole-ldlc.com>
2018-03-19 15:22:53 +0100

    added sprites

--- TD.py
+++ TD.py
@@ -17,7 +17,8 @@ GREEN = (0, 255, 0)
 BLUE = (0, 0, 255)
 YELLOW = (255, 255, 0)
 
-# initialize pygame and create window
+#xD
+# initialize Pygame and create window
 pygame.init()
 pygame.mixer.init()
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
