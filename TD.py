d43f777460cd57621da82583d1b03b6c3190ecda
theo.segard <theo.segard@lecole-ldlc.com>
2018-03-21 10:35:51 +0100

    fixed a bug about hp bar

--- TD.py
+++ TD.py
@@ -3,6 +3,7 @@ import random
 import os
 from os import path
 textures_dir = 'textures'
+animations_dir = 'animations'
 
 
 WIDTH = 480
@@ -26,6 +27,7 @@ pygame.display.set_caption("Rick's invaders")
 clock = pygame.time.Clock()
 
 font_name = pygame.font.match_font('arial')
+
 def draw_text(surf, text, size, x, y):
     font = pygame.font.Font(font_name, size)
     text_surface = font.render(text, True, WHITE)
@@ -59,6 +61,8 @@ class Player(pygame.sprite. Sprite):
         self.rect.bottom = HEIGHT - 10
         self.speedx = 0
         self.hp = 100
+        self.shoot_delay = 250
+        self.last_shot = pygame.time.get_ticks()
 
     def update(self):
         self.speedx = 0
@@ -67,6 +71,8 @@ class Player(pygame.sprite. Sprite):
             self.speedx = -8
         if keystate[pygame.K_RIGHT]:
             self.speedx = 8
+        if keystate[pygame.K_SPACE]:
+            self.shoot()
         self.rect.x += self.speedx
         if self.rect.right > WIDTH:
             self.rect.right = WIDTH
@@ -74,9 +80,12 @@ class Player(pygame.sprite. Sprite):
             self.rect.left = 0
 
     def shoot(self):
-        bullet = Bullet(self.rect.centerx, self.rect.top)
-        all_sprites.add(bullet)
-        bullets.add(bullet)
+        now = pygame.time.get_ticks()
+        if now - self.last_shot > self.shoot_delay:
+            self.last_shot = now
+            bullet = Bullet(self.rect.centerx, self.rect.top)
+            all_sprites.add(bullet)
+            bullets.add(bullet)
 
 class Mob(pygame.sprite.Sprite):
     def __init__(self):
@@ -167,7 +176,6 @@ while running:
     hits = pygame.sprite.spritecollide(player, mobs, True, pygame.sprite.collide_circle)
     if hits:
         player.hp -= 40
-        print(player.hp)
         mobrespawn()
     if player.hp <= 0:
         running = False
